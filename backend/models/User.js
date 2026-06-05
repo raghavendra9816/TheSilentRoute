e1563b7d81bc186739e81a514c7638ed21b04914
raghavendra9816 <raghavendra9816@gmail.com>
2026-06-05 20:49:33 +0545

    in user

--- backend/models/User.js
+++ backend/models/User.js
@@ -80,4 +80,4 @@ userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
