3bb2486a13950c44e18b84a6b7428b2306c80e54
raghavendra9816 <raghavendra9816@gmail.com>
2026-06-05 18:44:47 +0545

    chang

--- backend/models/user.js
+++ backend/models/user.js
@@ -57,7 +57,7 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
+// Hash password before saving to database
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(12);
